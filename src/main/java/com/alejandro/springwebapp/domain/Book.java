72f863a57211c2b578d6281fa338ee8ee8769ed8
Alejandro Andrade <alejo.andrade.rojas@gmail.com>
2021-05-15 18:35:21 -0300

    add BookController, add thymeleaf, remove circular reference books, authors.

--- src/main/java/com/alejandro/springwebapp/domain/Book.java
+++ src/main/java/com/alejandro/springwebapp/domain/Book.java
@@ -94,7 +94,6 @@ public class Book {
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
                 ", publisher=" + publisher +
-                ", authors=" + authors +
                 '}';
     }
 }
--- src/main/java/com/alejandro/springwebapp/domain/Publisher.java
+++ src/main/java/com/alejandro/springwebapp/domain/Publisher.java
@@ -56,7 +56,6 @@ public class Publisher {
                 ", city='" + city + '\'' +
                 ", state='" + state + '\'' +
                 ", zip='" + zip + '\'' +
-                ", books=" + books +
                 '}';
     }
 
